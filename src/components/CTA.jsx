077e2daa7828c121941baba963e4bcfcf1bb37c0
Thu Dang <>
2023-09-20 18:17:33 -0500

    add break in CTA

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -4,7 +4,7 @@ const CTA = () => {
   return (
     <section className="cta-bg py-[83px] xl:py-[76px]">
       <div className="w-container mx-auto text-center text-sacramentoGreen md:max-w-[573px] md:flexBetween xl:max-w-[917px]">
-        <h2 className="heading2 mb-[33px] md:mb-0">Ready to get started?</h2>
+        <h2 className="heading2 mb-[33px] md:mb-0">Ready to get <br className="md:hidden"/>started?</h2>
         <a href="/contact" className="cta cta-black">
           contact us
         </a>
